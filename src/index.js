b267d08391022ba571476270b40897dcf3d52331
barnescaleb <barnescaleb119@gmail.com>
2022-09-21 14:18:33 -0700

    added a tab switching module for each page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import { initialPageLoad } from './initial-page-load.js';
+import { menu } from './menu.js';
 
 initialPageLoad();
+
+// Tab Switcher Module
+
+let tabSwitcher = (function() {
+
+    const homeTab = document.querySelector(".home");
+    homeTab.addEventListener("click", initialPageLoad);
+
+    const menuTab = document.querySelector(".menu");
+    menuTab.addEventListener("click", menu);
+
+})();
